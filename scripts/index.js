09d5505bdda384a4ef77a519e99be1e55f0515a3
Cristian Sosa <cristiansosa1992@gmail.com>
2026-04-16 20:00:31 -0500

    primeraentrega

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// Datos iniciales
 const initialCards = [
   {
     name: "Valle de Yosemite",
@@ -25,7 +26,81 @@ const initialCards = [
   }
 ];
 
+// Recorrer tarjetas (debug)
 initialCards.forEach(function(item) {
   console.log(item.name);
   console.log(item.link);
 });
+
+// Selección de elementos
+const editButton = document.querySelector(".profile__edit-button");
+const editPopup = document.querySelector("#edit-popup");
+const closePopup = document.querySelector(".popup__close");
+const nameInput = document.querySelector(".popup__input_type_name");
+const descripcionInput = document.querySelector(".popup__input_type_description");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+//funciones reutilizables 
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+
+
+editButton.addEventListener("click", function () {
+  handleOpenEditModal()
+});
+
+
+closePopup.addEventListener("click", function () {
+  closeModal(editPopup);
+});
+
+
+//Obtengo los datos de nombre que muestra en pagina de nombre y description 
+function fillProfileForm() {
+  nameInput.value = profileTitle.textContent;
+  descripcionInput.value = profileDescription.textContent;
+}
+
+
+function handleOpenEditModal()
+{
+
+fillProfileForm();   // llena los inputs
+  openModal(editPopup); // abre el popup
+}
+
+// iniciamos editar perfil 
+
+// Vamos a buscar el formulario en el DOM
+let formElement = document.querySelector("form.popup__form");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  // ubico los inputs del form 
+  let nameInput = document.querySelector(".popup__input_type_name");
+  let jobInput = document.querySelector(".popup__input_type_description");
+
+  // obtengo datos
+  let nameValue = nameInput.value;
+  let jobValue = jobInput.value;
+
+  // selecciono donde voy a mostrar los datos
+  let profileTitle = document.querySelector(".profile__title");
+  let profileDescription = document.querySelector(".profile__description");
+
+  // Inserta nuevos valores utilizando la propiedad textContent
+
+  profileTitle.textContent = nameValue;
+  profileDescription.textContent = jobValue;
+}
+
+// conectar la función al formulario
+formElement.addEventListener("submit", handleProfileFormSubmit);
